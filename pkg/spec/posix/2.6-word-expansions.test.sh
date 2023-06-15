764db73ff3e10c0f017faadebdfbeb9d5f51b24f
Qi Xiao <xiaqqaix@gmail.com>
2023-06-15 21:58:50 +0100

    More tests for word expansions.

--- pkg/spec/posix/2.6-word-expansions.test.sh
+++ pkg/spec/posix/2.6-word-expansions.test.sh
@@ -1,13 +1,28 @@
 # TODO: Test the order of word expansions.
 
-#### Words expanding to a single null field are deleted
+#### Null words are deleted
 x=''
 printf ': %s\n' $x $(echo) $x$(echo) foo
 ## STDOUT:
 : foo
 ## END
 
-#### Single-quoted or partially single-quoted empty words are not deleted
+#### Null words are deleted even when IFS is empty
+IFS=''
+x=''
+printf ': %s\n' $x $(echo) $x$(echo) foo
+## STDOUT:
+: foo
+## END
+
+#### Non-null words expanding to a single null word is deleted
+x='   '
+printf ': %s\n' $x foo
+## STDOUT
+: foo
+## END
+
+#### Single-quoted or partially single-quoted null words are not deleted
 x=''
 printf ': %s\n' '' ''$x $x'' $(echo)'' ''$(echo) foo
 ## STDOUT:
@@ -19,7 +34,7 @@ printf ': %s\n' '' ''$x $x'' $(echo)'' ''$(echo) foo
 : foo
 ## END
 
-#### Double-quoted or partially double-quoted empty words are not deleted
+#### Double-quoted or partially double-quoted null words are not deleted
 x=''
 printf ': %s\n' "" ""$x $x"" ""$(echo) $(echo)"" foo
 ## STDOUT:
