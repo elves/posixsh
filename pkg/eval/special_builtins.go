040d909ea35fd1a360dbabb54f8e824013d8c6ef
Qi Xiao <xiaqqaix@gmail.com>
2023-06-30 14:32:38 +0100

    Refactor

--- pkg/eval/special_builtins.go
+++ pkg/eval/special_builtins.go
@@ -38,54 +38,6 @@ func breakCmd(fm *frame, args []string) (int, bool) {
 	return abortLoop(fm, args, false)
 }
 
-// Implements break and continue. This works by returning (0, false) after
-// setting fm.loopAbort, which is examined in the implementation of
-// for/while/until.
-func abortLoop(fm *frame, args []string, next bool) (int, bool) {
-	level, ok := parseOneInt(fm, args, 1)
-	if !ok {
-		return StatusBadCommandLine, false
-	}
-	if level <= 0 {
-		fm.badCommandLine("argument must be positive, got %v", level)
-		return StatusBadCommandLine, false
-	}
-	if fm.loopDepth == 0 {
-		// POSIX leaves the behavior of break and continue unspecified when
-		// there is no enclosing loop. We just let it do nothing; this is
-		// consistent with the behavior when n is greater than the number of
-		// enclosing loops.
-		//
-		// This behavior is shared by dash and ksh, but not by bash and zsh.
-		return 0, true
-	}
-	dest := fm.loopDepth - level
-	// POSIX specifies that break and continue should work on the outermost
-	// loop when n > number of enclosing loops.
-	if dest < 0 {
-		dest = 0
-	}
-	fm.loopAbort = &loopAbort{dest: dest, next: next}
-	return 0, false
-}
-
-func parseOneInt(fm *frame, args []string, fallback int) (int, bool) {
-	switch len(args) {
-	case 0:
-		return fallback, true
-	case 1:
-		n, err := strconv.Atoi(args[0])
-		if err != nil {
-			fm.badCommandLine("argument must be number, got %q", args[0])
-			return 0, false
-		}
-		return n, true
-	default:
-		fm.badCommandLine("at most 1 argument accepted, got %v", len(args))
-		return 0, false
-	}
-}
-
 func colon(*frame, []string) (int, bool) {
 	return 0, true
 }
@@ -182,4 +134,54 @@ func unset(fm *frame, args []string) (int, bool) {
 		}
 	}
 	return 0, true
+}
+
+// Utilities used for implementing special builtins.
+
+// Implements break and continue. This works by returning (0, false) after
+// setting fm.loopAbort, which is examined in the implementation of
+// for/while/until.
+func abortLoop(fm *frame, args []string, next bool) (int, bool) {
+	level, ok := parseOneInt(fm, args, 1)
+	if !ok {
+		return StatusBadCommandLine, false
+	}
+	if level <= 0 {
+		fm.badCommandLine("argument must be positive, got %v", level)
+		return StatusBadCommandLine, false
+	}
+	if fm.loopDepth == 0 {
+		// POSIX leaves the behavior of break and continue unspecified when
+		// there is no enclosing loop. We just let it do nothing; this is
+		// consistent with the behavior when n is greater than the number of
+		// enclosing loops.
+		//
+		// This behavior is shared by dash and ksh, but not by bash and zsh.
+		return 0, true
+	}
+	dest := fm.loopDepth - level
+	// POSIX specifies that break and continue should work on the outermost
+	// loop when n > number of enclosing loops.
+	if dest < 0 {
+		dest = 0
+	}
+	fm.loopAbort = &loopAbort{dest: dest, next: next}
+	return 0, false
+}
+
+func parseOneInt(fm *frame, args []string, fallback int) (int, bool) {
+	switch len(args) {
+	case 0:
+		return fallback, true
+	case 1:
+		n, err := strconv.Atoi(args[0])
+		if err != nil {
+			fm.badCommandLine("argument must be number, got %q", args[0])
+			return 0, false
+		}
+		return n, true
+	default:
+		fm.badCommandLine("at most 1 argument accepted, got %v", len(args))
+		return 0, false
+	}
 }
