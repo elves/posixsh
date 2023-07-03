391775597df47a124c50cd9842250263fe5c173a
Qi Xiao <xiaqqaix@gmail.com>
2023-07-03 10:40:57 +0100

    Add a test for the effect of nountest

--- pkg/spec/posix/set-nounset.test.sh
+++ pkg/spec/posix/set-nounset.test.sh
@@ -1,4 +1,11 @@
-# The effect of set -o nounset is tested in 2.6.2-parameter-expansion.test.sh.
+# Most of the tests for the effect of set -o nounset are in
+# 2.6.2-parameter-expansion.test.sh.
+
+#### set -o nounset also affects arithmetic expansion
+set -o nounset
+echo $(( x ))
+## status: [1, 127]
+## stderr-regexp: .+
 
 #### set -u is equivalent to set -o nounset
 set -u
